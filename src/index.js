392b1e79833d40d6660bdb48b3ffadd3db3d3819
An Khong <andkhong@gmail.com>
2017-03-04 19:38:30 -0800

    removed webpack from express file

--- src/index.js
+++ src/index.js
@@ -2,21 +2,25 @@ const Server = require('./server/server.js');
 const port = (process.env.PORT || 8080);
 const app = Server.app();
 
-if (process.env.NODE_ENV !== 'production') {
-  const webpack = require('webpack');
-  const webpackMiddleware = require('webpack-dev-middleware');
-  const webpackHotMiddleware = require('webpack-hot-middleware');
-  const webpackConfig = require('./../webpack.dev.config');
-
-  const compiler = webpack(webpackConfig);
-  app.use(webpackMiddleware(compiler, {
-    hot: true,
-    publicPath: webpackConfig.output.publicPath,
-    noInfo: true
-  }));
-  app.use(webpackHotMiddleware(compiler));
-
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   const webpack = require('webpack');
+//   const webpackMiddleware = require('webpack-dev-middleware');
+//   const webpackHotMiddleware = require('webpack-hot-middleware');
+//   const webpackConfig = require('./../webpack.dev.config');
+//
+//   const compiler = webpack(webpackConfig);
+//   app.use(webpackMiddleware(compiler, {
+//     hot: true,
+//     publicPath: webpackConfig.output.publicPath,
+//     noInfo: true
+//   }));
+//   app.use(webpackHotMiddleware(compiler));
+//
+//   // app.listen(port, function(){
+//   //   console.log(`Development Mode: Listening at http://localhost:`, port)
+//   // })
+//
+// }
 
 app.listen(port, function(){
   console.log(`Build Mode: Listening at http://localhost:`, port)
